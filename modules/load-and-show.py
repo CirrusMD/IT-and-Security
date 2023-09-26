5e12b7c50ca72a49615d9f50e3de13b6b47724b4
Austin Songer <austin-songer@cirrusmd.com>
2023-09-26 10:41:37 -0500

    Update load-and-show.py

--- modules/load-and-show.py
+++ modules/load-and-show.py
@@ -1,13 +1,31 @@
 import pandas as pd
 
 # Load the CSV files into DataFrames
-user_list_df = pd.read_csv('/jcuserlist_20230925.csv')
-user_groups_df = pd.read_csv('/users-to-user-groups-6511da5f8d205ef0d9bcdbe7-20230925Z.csv')
-user_apps_df = pd.read_csv('/users-to-sso-applications-6511daca2624fcca7fea9f4c-20230925Z.csv')
+user_list_df = pd.read_csv('path_to_user_list.csv')
+user_groups_df = pd.read_csv('path_to_user_groups.csv')
+user_apps_df = pd.read_csv('path_to_user_apps.csv')
 
 # Show the first few rows of each DataFrame to get an idea of the structure
 user_list_preview = user_list_df.head()
 user_groups_preview = user_groups_df.head()
 user_apps_preview = user_apps_df.head()
+print("User List Preview:")
+print(user_list_preview)
+print("User Groups Preview:")
+print(user_groups_preview)
+print("User Apps Preview:")
+print(user_apps_preview)
 
-user_list_preview, user_groups_preview, user_apps_preview
+# Summarize the columns and their data types for each DataFrame
+user_list_columns = user_list_df.dtypes
+user_groups_columns = user_groups_df.dtypes
+user_apps_columns = user_apps_df.dtypes
+print("User List Columns and Data Types:")
+print(user_list_columns)
+print("User Groups Columns and Data Types:")
+print(user_groups_columns)
+print("User Apps Columns and Data Types:")
+print(user_apps_columns)
+
+if __name__ == '__main__':
+    main()
