4561a4cb38f2e65067a237bf3b9372ccde1a3cf7
Austin Songer <austin-songer@cirrusmd.com>
2023-09-26 11:35:22 -0500

    Update access-review.py

--- access-review.py
+++ access-review.py
@@ -61,24 +61,66 @@ def main():
     doc.add_paragraph(f'High-Risk Combinations:')
     doc.add_paragraph(f'{high_risk_combinations.head()}')
 
-# Ask the user if they want to export additional CSV files
+
 export_csv_option = input("Do you want to export additional CSV files? (y/n): ")
 if export_csv_option.lower() == 'y':
-    # 1) List of users and the number of groups assigned, and another column that lists all the groups
-    user_group_count = user_groups_df.groupby('username')['user_group_name'].agg(['count', lambda x: ', '.join(x)]).reset_index()
-    user_group_count.columns = ['Username', 'Number_of_Groups', 'List_of_Groups']
-    user_group_count.to_csv('User_Group_Info.csv', index=False)
+    print("Choose which additional CSV files you want to export:")
+    print("1: List of users and the number of groups assigned, and another column that lists all the groups")
+    print("2: List of Groups and the total number of users")
+    print("3: Inactive Users With Group Access")
+    print("4: Groups With Sensitive App Access")
+    print("5: Users Without Two-Factor Authentication")
+    print("6: Rarely Used Apps")
+    print("7: Users With Admin Privileges")
+    print("8: Data Completeness Report")
+    print("9: Expired Account Credentials")
+    selected_options = input("Enter the numbers of the options you want, separated by commas (e.g., 1,3,5): ").split(",")
     
-    # 2) List of Groups and the total number of users
-    group_user_count = user_groups_df.groupby('user_group_name')['username'].count().reset_index()
-    group_user_count.columns = ['Group_Name', 'Number_of_Users']
-    group_user_count.to_csv('Group_User_Info.csv', index=False)
+    if '1' in selected_options:
+        # 1) List of users and the number of groups assigned, and another column that lists all the groups
+        user_group_count = user_groups_df.groupby('username')['user_group_name'].agg(['count', lambda x: ', '.join(x)]).reset_index()
+        user_group_count.columns = ['Username', 'Number_of_Groups', 'List_of_Groups']
+        user_group_count.to_csv('User_Group_Info.csv', index=False)
     
-    print("CSV files have been exported.")
+    if '2' in selected_options:
+        # 2) List of Groups and the total number of users
+        group_user_count = user_groups_df.groupby('user_group_name')['username'].count().reset_index()
+        group_user_count.columns = ['Group_Name', 'Number_of_Users']
+        group_user_count.to_csv('Group_User_Info.csv', index=False)    
 
+    if '3' in selected_options:
+        # 3) Inactive Users With Group Access
+        inactive_users_with_groups.to_csv('Inactive_Users_With_Group_Access.csv', index=False)
+        
+    if '4' in selected_options:
+        # 4) Groups With Sensitive App Access
+        groups_with_sensitive_apps.to_csv('Groups_With_Sensitive_App_Access.csv', index=False)
+        
+    if '5' in selected_options:
+        # 5) Users Without Two-Factor Authentication
+        user_list_df.to_csv('Users_Without_Two_Factor_Authentication.csv', index=False)
+        
+    if '6' in selected_options:
+        # 6) Rarely Used Apps
+        rarely_used_apps.to_csv('Rarely_Used_Apps.csv', index=False)
+        
+    if '7' in selected_options:
+        # 7) Users With Admin Privileges
+        users_with_admin_privileges.to_csv('Users_With_Admin_Privileges.csv', index=False)
+        
+    if '8' in selected_options:
+        # 8) Data Completeness Report
+        data_completeness_report.to_csv('Data_Completeness_Report.csv', index=False)
+        
+    if '9' in selected_options:
+        # 9) Expired Account Credentials
+        expired_account_credentials.to_csv('Expired_Account_Credentials.csv', index=False)
+        
+    print("Selected CSV files have been exported.")        
     
     # Save the document
-    doc.save('Access_Review_Report.docx')
+    current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    doc.save(f'Access_Review_Report_{current_time}.docx')
 
 if __name__ == '__main__':
     main()
