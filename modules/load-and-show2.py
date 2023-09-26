97bd586f7ae5e1535148b27e8e576629d7d9cb57
Austin Songer <austin-songer@cirrusmd.com>
2023-09-26 10:40:58 -0500

    Delete modules/load-and-show2.py

--- modules/load-and-show2.py
+++
@@ -1,7 +0,0 @@
-# Summarize the columns and their data types for each DataFrame
-
-user_list_columns = user_list_df.dtypes
-user_groups_columns = user_groups_df.dtypes
-user_apps_columns = user_apps_df.dtypes
-
-user_list_columns, user_groups_columns, user_apps_columns
